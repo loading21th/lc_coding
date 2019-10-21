9c2c56f937b1da7ca0477115deb0ae163d4b02cf
liwei0526vip <liwei0526vip@163.com>
2019-10-21 23:08:05 +0800

    list: add api

--- list/list.c
+++ list/list.c
@@ -21,6 +21,71 @@ void list_deinit(list_t *list)
 }
 
 
+node_t *list_find_by_index(list_t *list, int index)
+{
+    if (NULL == list || 0 == list->size || index >= list->size) {
+        return NULL;
+    }
+
+    node_t *node = list->head;
+    while(node && index--) {
+        node = node->next;
+    }
+    return node;
+}
+
+
+node_t *list_find_by_value(list_t *list, int value)
+{
+    if (NULL == list || 0 == list->size) {
+        return NULL;
+    }
+
+    node_t *node = list->head;
+    while(node && node->data != value) {
+        node = node->next;
+    }
+
+    return node;
+}
+
+
+list_t *list_insert(list_t *list, int index, int data)
+{
+    if (NULL == list || index > list->size) {
+        return NULL;
+    }
+    node_t *new_node = malloc(sizeof(node_t));
+    if (NULL == new_node) {
+        return NULL;
+    }
+    new_node->data = data;
+
+    if (0 == list->size) {
+        list->head = new_node;
+        list->tail = new_node;
+        new_node->next = NULL;
+    }
+
+    node_t *node = list->head;
+    node_t *pre  = node;
+    while(node && index--) {
+        pre  = node;
+        node = node->next;
+    }
+    if (node == list->head) {
+        new_node->next = node;
+        list->head = new_node;
+    } else {
+        pre->next = new_node;
+        new_node->next = node;
+    }
+    list->size++;
+
+    return list;
+}
+
+
 list_t *list_add_node_head(list_t *list, int data)
 {
     if (NULL == list) {
@@ -145,7 +210,10 @@ int main(int argc, char *argv[])
     list_add_node_tail(list, 5);
     list_add_node_tail(list, 6);
     list_del_node_tail(list);
+    list_add_node_head(list, 100);
+    list_insert(list, 0, 222);
     list_show(list);
+    printf("%d data is %d\n", 2, list_find_by_index(list, 2)->data);
     printf("list length = %zu\n", list_length(list));
     list_deinit(list);
 }
